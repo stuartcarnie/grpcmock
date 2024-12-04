d0d90380a43c183543641e16ca14f14cb14afc74
declark1 <44146800+declark1@users.noreply.github.com>
2024-12-04 11:36:23 -0800

    fix: use fully-qualified types in macro to avoid import errors when generating multiple mock servers
    Signed-off-by: declark1 <44146800+declark1@users.noreply.github.com>

--- grpcmock/src/codegen.rs
+++ grpcmock/src/codegen.rs
@@ -2,50 +2,40 @@
 #[macro_export]
 macro_rules! generate_server {
     ($name:literal, $type:ident) => {
-        use std::{
-            ops::{Deref, DerefMut},
-            task::Poll,
-        };
-        use tonic::codegen::{http, Body, BoxFuture, Service, StdError};
-
-        use $crate::mock::MockSet;
-        use $crate::server::MockServer;
-        use $crate::Error;
-
         #[derive(Clone)]
-        pub struct $type(MockServer);
+        pub struct $type($crate::server::MockServer);
 
-        impl Deref for $type {
-            type Target = MockServer;
+        impl std::ops::Deref for $type {
+            type Target = $crate::server::MockServer;
 
             fn deref(&self) -> &Self::Target {
                 &self.0
             }
         }
 
-        impl DerefMut for $type {
+        impl std::ops::DerefMut for $type {
             fn deref_mut(&mut self) -> &mut Self::Target {
                 &mut self.0
             }
         }
 
-        impl<B> Service<http::Request<B>> for $type
+        impl<B> tonic::codegen::Service<tonic::codegen::http::Request<B>> for $type
         where
-            B: Body + Send + 'static,
+            B: tonic::codegen::Body + Send + 'static,
             B::Data: Send,
-            B::Error: Into<StdError> + Send + std::fmt::Debug + 'static,
+            B::Error: Into<tonic::codegen::StdError> + Send + std::fmt::Debug + 'static,
         {
-            type Response = http::Response<tonic::body::BoxBody>;
+            type Response = tonic::codegen::http::Response<tonic::body::BoxBody>;
             type Error = std::convert::Infallible;
-            type Future = BoxFuture<Self::Response, Self::Error>;
+            type Future = tonic::codegen::BoxFuture<Self::Response, Self::Error>;
 
             fn poll_ready(
                 &mut self,
                 _cx: &mut std::task::Context<'_>,
-            ) -> Poll<std::result::Result<(), Self::Error>> {
-                Poll::Ready(Ok(()))
+            ) -> std::task::Poll<std::result::Result<(), Self::Error>> {
+                std::task::Poll::Ready(Ok(()))
             }
-            fn call(&mut self, req: http::Request<B>) -> Self::Future {
+            fn call(&mut self, req: tonic::codegen::http::Request<B>) -> Self::Future {
                 self.0.handle(req)
             }
         }
@@ -55,8 +45,8 @@ macro_rules! generate_server {
         }
 
         impl $type {
-            pub async fn start(mocks: MockSet) -> Result<Self, Error> {
-                let server = MockServer::new($name, mocks)?;
+            pub async fn start(mocks: $crate::mock::MockSet) -> Result<Self, $crate::Error> {
+                let server = $crate::server::MockServer::new($name, mocks)?;
                 Ok(Self(server).serve().await)
             }
 
